91c907dbf175da1bcbda850634a40a5e61302ae6
Julia Lopes <julialopes@usp.br>
2024-10-19 23:40:58 -0300

    criando html na função do index.jsx

--- cadastros-react/src/pages/Home/index.jsx
+++ cadastros-react/src/pages/Home/index.jsx
@@ -3,8 +3,17 @@ import "./style.css"
 
 function Home(){
     return (
-    <div>
-        <h1>ola</h1>
+    <div className="container">
+        <form>
+            <h1>Castro de usuários</h1>
+            <input name="nome" type="text" />
+            <input name="idade" type="number" />
+            <input name="email" type="email" />
+            <button type="button">Cadastrar</button>
+        </form>
+
+
+        
     </div>
     )
 }
